ca938b1f6ce306d59bfd2f3c54801d6f166387eb
Betty985 <1830265753@qq.com>
2023-01-09 20:13:53 +0800

    alert

--- src/components/Alert/index.stories.tsx
+++ src/components/Alert/index.stories.tsx
@@ -4,9 +4,7 @@ import {Alert} from "./index";
 export default {
    title: "Alert"
 };
-    
+const onClose=()=>console.log('close')
 export const WithAlert = () => (
-  <Alert>
-     This is my Alert component
-  </Alert>
+  <Alert type='primary' title='title' description='description' closeable onClose={onClose}/>
 );
--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
-import './index.css'
+import React, { ReactNode, FC, useState, MouseEventHandler } from 'react';
+import classnames from 'classnames';
+import './index.css';
 export interface AlertProps {
-    children?: React.ReactNode
-  }
-const Alert = ({ children }: AlertProps) => {
-    return (
-    <div className='Alert'>
-       {children}
-    </div>
-   )
-  }
-export {Alert}
-     
+  /** 标题 */
+  title?: string;
+  /** 内容 */
+  description?: ReactNode;
+  /** 类型 */
+  type: 'success' | 'primary' | 'warning' | 'danger' | 'default';
+  /** 默认不显示关闭按钮 */
+  closeable?: boolean;
+  /** 关闭icon */
+  closeIcon?: ReactNode;
+  /** 关闭时触发的回调函数 */
+  onClose?: React.MouseEventHandler<HTMLButtonElement>;
+}
+const Alert: FC<AlertProps> = props => {
+  const { title, description, closeIcon = 'X', type, onClose, closeable = false } = props;
+  const [visible, setVisible] = useState(true);
+  const classes = classnames('alert', { [`alert-${type}`]: true });
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setVisible(false);
+    onClose?.(e);
+  };
+  return (
+    <>
+      {visible && (
+        <div className={classes}>
+          {title && <p className="alert-title">{title}</p>}
+          {description && <p className="alert-description">{description}</p>}
+          {closeable && <button className='alert-close' onClick={handleClick}>{closeIcon}</button>}
+        </div>
+      )}
+    </>
+  );
+};
+export { Alert };
